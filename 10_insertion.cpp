8b0c4c121edbf721d189a747e5a91a5737102e7e
Saksham Sharma <87091886+sakkshm26@users.noreply.github.com>
2021-11-14 21:13:40 +0530

    Add files via upload

---
+++ 10_insertion.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void display(int arr[], int n)
+{
+    // Code for traversal
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int indInsertion(int arr[], int size, int element, int capacity, int index)
+{
+    // Code for insertion
+    if(size>=capacity)
+    {
+        return -1;
+    }
+
+    for (int i=size-1; i>=index; i--)
+    {
+        arr[i+1]=arr[i];
+    }
+    arr[index]=element;
+    return 0;
+}
+
+int main()
+{
+    int arr[100]={7,8,12,27,88};
+    int size=5,element=45,index=3;
+    display(arr,size);
+    cout<<endl;
+    indInsertion(arr,size,element,100,index);
+    size+=1;
+    display(arr,size); 
+    return 0;
+}
